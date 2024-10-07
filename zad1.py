2d097639c8b31ba20fda8e9996b3a75bd8032585
baranhead <fwbaran@gmail.com>
2024-10-07 16:06:47 +0200

    zadanie 1 - estymacja liczby pi metoda Monte Carlo

--- zad1.py
+++ zad1.py
@@ -1,2 +1,27 @@
-x = 3
-print (x+1)
+import math
+import random
+
+def estimate(i):
+    inside = 0
+
+    for j in range(i):
+        x = random.uniform(0, 1)
+        y = random.uniform(0, 1)
+
+        if(x**2 + y**2 <= 1):
+            inside += 1
+
+    return (inside/i) * 4
+
+n = int(input("Podaj liczbe iteracji\n"))
+k = int(input("Podaj krok\n"))
+
+for i in range(k, n+1, k):
+    pi_est = estimate(i)
+    print(f"Przyblizenie pi po {i} iteracjach: {pi_est}")
+
+pi_est = estimate(n)
+diff = abs(math.pi - pi_est)
+
+print(f"Roznica miedzy wartoscia estymowana po {n} iteracjach i wartoscia math.pi: {diff}")
+
